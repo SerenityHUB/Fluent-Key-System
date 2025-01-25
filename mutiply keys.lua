ba5d8d4504e5d07693235c737982a2b3d94ffdd4
SerenityHUB <ecsclanmail@gmail.com>
2025-01-25 16:44:51 +0200

    Update mutiply keys.lua

--- mutiply keys.lua
+++ mutiply keys.lua
@@ -6,7 +6,7 @@ local Window = Fluent:CreateWindow({
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
     Theme = "Darker", 
-    MinimizeKey = Enum.KeyCode.LeftControl
+    MinimizeKey = Enum.KeyCode.K
 })
 
 local Tabs = {
@@ -25,7 +25,7 @@ Tabs.Key:AddInput("KeyInput", {
     Callback = function(Value)
         if table.find(Keys, Value) then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/AirHub.lua"))()
-													                      -- // ^ Set your script to load
+													-- // ^ Set your script to load
             Window:Destroy()
         end
     end
@@ -36,7 +36,7 @@ Tabs.Key:AddButton({
     Description = "No Linkvertise/Lootlabs",
     Callback = function()
         setclipboard("https://discord.gg/gmBxyhkR")
-				        -- // ^ Set your discord server here
+				-- // ^ Set your discord server here
         Fluent:Notify({
             Title = "Copied!",
             Content = "Copied Link to clipboard!",
